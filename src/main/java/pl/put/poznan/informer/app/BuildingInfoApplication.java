13af70af93ba4c6b946a25ae0935436cf3f51273
x-Yoshida <69041080+x-Yoshida@users.noreply.github.com>
2023-12-16 12:51:20 +0100

    Added JavaDoc

--- src/main/java/pl/put/poznan/informer/app/BuildingInfoApplication.java
+++ src/main/java/pl/put/poznan/informer/app/BuildingInfoApplication.java
@@ -3,10 +3,17 @@ package pl.put.poznan.informer.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Glowna klasa projektu, odpowiedzialna za uruchamianie programu
+ * @version 0.1
+ * @author Gamma
+*/
 
 @SpringBootApplication(scanBasePackages = {"pl.put.poznan.informer.rest"})
 public class BuildingInfoApplication {
-
+    /**
+     * @hidden
+     */
     public static void main(String[] args) {
         SpringApplication.run(BuildingInfoApplication.class, args);
     }
--- src/main/java/pl/put/poznan/informer/logic/BazaDanych.java
+++ src/main/java/pl/put/poznan/informer/logic/BazaDanych.java
@@ -2,6 +2,11 @@ package pl.put.poznan.informer.logic;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Implenentacja BazyDanych
+ * @version 0.1
+ * @author Gamma
+ */
 public class BazaDanych {
     private List<Lokacja> obiekty = new ArrayList<>();
 
@@ -9,6 +14,10 @@ public class BazaDanych {
         this.obiekty.add(lokacja);
     }
 
+    /**
+     * @param id - id szukanej lokalizacji
+     * @return jeśli istnieje, zwraca lokalizacje o szukanym id
+     */
     public Lokacja getObjectById(int id) {
         for (Lokacja lokacja : obiekty) {
             if (lokacja.getId() == id) {
