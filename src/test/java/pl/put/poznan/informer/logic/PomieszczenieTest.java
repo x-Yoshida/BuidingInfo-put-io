68ba4b7d951ec8067961c9b2cc4e6c07f3fd4c95
unknown <krzysztof.bartczak@student.put.poznan.pl>
2024-01-22 02:54:20 +0100

    dodanie klasy testującej33

--- src/test/java/pl/put/poznan/informer/logic/PomieszczenieTest.java
+++ src/test/java/pl/put/poznan/informer/logic/PomieszczenieTest.java
@@ -1,4 +1,49 @@
+package pl.put.poznan.informer.logic;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
+
 class PomieszczenieTest {
-  
+
+    private Pomieszczenie pomieszczenie;
+    private Poziom p_mock;
+
+    @BeforeEach
+    void setup(){
+        p_mock = mock(Poziom.class);
+        when(p_mock.getId()).thenReturn(1);
+        pomieszczenie = new Pomieszczenie(1,"pomieszczenie");
+    }
+    @Test
+    void getPowierzchnia() {
+        assertNotNull(pomieszczenie.getPowierzchnia());
+    }
+
+    @Test
+    void getKubatura() {
+        assertNotNull(pomieszczenie.getKubatura());
+    }
+
+    @Test
+    void getLight() {
+        assertEquals(pomieszczenie.getLightPower()/ pomieszczenie.getKubatura(), pomieszczenie.getLight());
+    }
+
+    @Test
+    void getLightPower() {
+        assertNotNull(pomieszczenie.getLightPower());
+    }
+
+    @Test
+    void getHeating() {
+        assertEquals(pomieszczenie.getHeatingPower()/ pomieszczenie.getKubatura(), pomieszczenie.getHeating());
+    }
+
+    @Test
+    void mocktest(){
+        assertEquals(p_mock.getId(), 1);
+    }
 }
